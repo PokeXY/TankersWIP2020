37e1751fcd0fae15a25027438927a930e822ccb8
unknown <tanner-olsen@hotmail.com>
2020-06-15 22:58:24 -0400

    Basic stats created, camera resolution set, player script and marisa script created

---
+++ Touhou Tankers/Assets/Scripts/MarisaStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarisaStats : MonoBehaviour
+{
+    // Marisa's Stats
+    public int power = 5;
+    public int defense = 2;
+    public int speed = 3;
+    public int bombPower = 10;
+    public int exp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
